3e821bebb9d0f7b4da125ed157e856027a19020a
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2022-11-22 00:22:03 -0300

    add tp4

---
+++ tp4/tp4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib_conjunto.h"
+#define MAX 100
+
+int main ()
+{
+
+    /* ler os herois e suas habilidades */
+
+    /* ler a missao */
+
+    /* ler as equipes de herois */
+
+    /* a solucao eh encontrada se a missao esta contido na uniao das 
+     * habilidades de uma equipe, mas tomando-se aquela de menor tamanho. */
+
+    /* libera toda a memoria alocada dinamicamente */
+
+    return 0;
+}
