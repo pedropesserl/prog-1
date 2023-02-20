0db56584d3818d1d4ea18dff1ac1fc8aaeb1ec05
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-02-19 23:00:43 -0300

    part 1 done

--- tshell/tshell_p1.sh
+++ tshell/tshell_p1.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+if [ $# -lt 3 ] || [ $# -gt 5 ]; then
+	>&2 echo "Uso: <dpm> <ds> <t1> [<t2> [<t3>]]"
+	exit 1
+fi
+
+FONTE=$1
+DESTINO=$2
+T1=$3
+T2=$4
+T3=$5
+
+if [ ! -d $FONTE ]; then
+	>&2 echo "Erro: $FONTE não é um diretório."
+	exit 1
+fi
+
+if [ ! -d $DESTINO ]; then
+	mkdir $DESTINO
+fi
+
+DESTINO=$(realpath $DESTINO)
+
+cd $FONTE
+for ARQ in *; do
+	awk '(NR>1)' $ARQ | grep -iF "$T1" | grep -iF "$T2" | grep -iF "$T3" > $DESTINO/$ARQ
+done
+cd - > /dev/null
