352636f3e9ad1ac2573587b6f1beced38eb67bfd
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-02-19 23:02:00 -0300

    start part 2

--- tshell/tshell_p2.sh
+++ tshell/tshell_p2.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+if [ $# -ne 2 ]; then
+	>&2 echo "Uso: <dcsv> <dsin>"
+	exit 1
+fi
+
+testa_se_diretorio() {
+	if [ ! -d $1 ]; then
+		>&2 echo "Erro: $1 não é um diretório."
+		exit 1
+	fi
+}
+
+DCSV=$1
+DSIN=$2
+
+testa_se_diretorio $DCSV
+testa_se_diretorio $DSIN
